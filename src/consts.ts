3ce88d165a8ce1b91ef97c93b208814730dcf60e
aufschrittundtritt <aufschrittundtritt@immerda.ch>
2026-02-07 03:39:43 +0100

    build: add github workflow

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const site = "https://aufschrittundtritt.ch";
+export const site = "https://aufschrittundtritt.github.io";
 export const siteTitle = "Auf Schritt und Tritt";
